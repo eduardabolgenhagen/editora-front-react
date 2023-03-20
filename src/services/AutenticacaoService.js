26be153cecd31e504e7082f909baeb9fb8d03e8a
Eduarda Bolgenhagen <eduarda_b_campos@estudante.sesisenai.org.br>
2023-03-19 22:52:14 -0300

    :+1:

---
+++ src/services/AutenticacaoService.js
@@ -0,0 +1,18 @@
+// Biblioteca que permite fazer requisições HTTP
+import axios from "axios";
+
+// Endpoint para a autenticação na API
+const url = "https://localhost:8443/login"
+
+class AutenticacaoService {
+    login(user){
+        const config = {
+            // Define que a aplicação que estiver fazendo a requisição deve enviar cookies ou token
+            withCredentials: true
+        };
+        console.log("Chamou o método")
+        return axios.post(url + "/auth", user, config);
+    }
+}
+
+export default new AutenticacaoService();
